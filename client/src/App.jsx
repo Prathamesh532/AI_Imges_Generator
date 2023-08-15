0cd5d4e4f803211bbdd2418dd1f8da69e25a6956
Prathamesh532 <prathameshkalekar0532@gmail.com>
2023-08-15 14:36:53 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Link ,Route ,Routes } from 'react-router-dom'
-import {logo} from "./assets"
+import {Logo} from "./assets"
 import { Home, CreatePost } from './pages'
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
        <BrowserRouter>
           <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]'>
             <Link to="/">
-              <img src={ logo } alt="Logo" className='w-28 object-contain'/>
+              <img src={ Logo } alt="Logo" className='w-28 object-contain'/>
             </Link>
 
             <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import Logo from './Logo.png'
 
 export {
   download,
   logo,
   preview,
+  Logo
 };
